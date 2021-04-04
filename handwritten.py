111f2a585c9d132120fb6810b2bfc30eb61be81f
Bhaskar <bhaskar1208200r@gmail.com>
2021-04-05 04:13:31 +0530

    Training the model

--- handwritten.py
+++ handwritten.py
@@ -59,3 +59,17 @@ model.compile(loss = 'categorical_crossentropy',
               metrics = ['accuracy'])
 
 print(model.summary())
+
+batch_size = 128
+epochs = 10
+
+#Training
+history = model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
